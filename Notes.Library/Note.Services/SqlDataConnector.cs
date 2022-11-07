139f9c15d5d1180f49c429327510c5d75bfce745
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-07 18:36:12 +0400

    DataConnector

---
+++ Notes.Library/Note.Services/SqlDataConnector.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using Note.Services.Interfaces;
+using Notes.Library;
+using System.Data;
+
+namespace Note.Services
+{
+    internal class SqlDataConnector : IDataConnection
+    {
+        public async Task<List<NotesCS>> GetAllNotes()
+        {
+            const string sqlExpression = "sp_SelectedNotes";
+            List<NotesCS> result = new();
+
+            using (SqlConnection connection = new(GlobalConfig.ConnectionString()))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand sqlCmd = new(sqlExpression, connection);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader dataReader = await sqlCmd.ExecuteReaderAsync();
+
+                    if (dataReader.HasRows)
+                    {
+                        while (await dataReader.ReadAsync())
+                        {
+                            result.Add(new NotesCS
+                            {
+                                NoteId = dataReader.GetInt32(0),
+                                NoteHeader = dataReader.GetString(1),
+                                NoteDescr = dataReader.GetString(2),
+                                NoteDate = dataReader.GetDateTime(3)
+                            });
+                        }
+                    }
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+                finally
+                {
+                   await connection.CloseAsync();
+                }
+            }
+
+            return result;
+        }
+    }
+}
