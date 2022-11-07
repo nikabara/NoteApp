259adf60b0a99e3253ed5d56861cdf163ad7c7d8
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-07 18:36:35 +0400

    Notes

---
+++ Notes.Library/Notes.Library/NotesCS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Library
+{
+    public class NotesCS
+    {
+        public int NoteId { get; set; }
+        public string? NoteHeader { get; set; }
+        public string? NoteDescr { get; set; }
+        public DateTime NoteDate { get; set; }
+    }
+}
