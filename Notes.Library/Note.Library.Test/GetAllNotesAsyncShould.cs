d8c169780df314a7803e06be59287344989a3169
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-07 21:08:33 +0400

    Note Tests

---
+++ Notes.Library/Note.Library.Test/GetAllNotesAsyncShould.cs
@@ -0,0 +1,30 @@
+using Xunit;
+using Note.Services;
+using Notes.Library;
+using System.Collections.Generic;
+using System;
+
+namespace Note.Library.Test
+{
+    public class GetAllNotesAsyncShould
+    {
+        [Fact]
+        public async void Return_All_Notes_From_Database()
+        {
+            SqlDataConnector sqlDataConnector = new();
+
+            List<NotesCS> expected = new()
+            {
+                new NotesCS()
+                {
+                    NoteId = 1,
+                    NoteHeader = "Test",
+                    NoteDescr = "This is test note for C# =)",
+                    NoteDate = DateTime.Now
+                }
+            };
+
+            List<NotesCS> actual = await sqlDataConnector.GetAllNotesAsync();
+        }
+    }
+}
