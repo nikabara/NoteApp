cd15dea80b7734a92eb197878b82361bdb30b8ad
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-07 18:35:38 +0400

    GlobalConfig

---
+++ Notes.Library/Note.Services/GlobalConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note.Services
+{
+    internal class GlobalConfig
+    {
+        public static string ConnectionString()
+        {
+            return @"Server=DESKTOP-N93AKIG\SQLEXPRESS;Database=SqlNote;Trusted_Connection=True;TrustedServerCertificate=True";
+        }
+    }
+}
