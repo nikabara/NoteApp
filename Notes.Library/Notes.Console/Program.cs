06146a1c8d48431aa37e50835da593e56bc5255f
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-08 20:41:41 +0400

    Console Debugger

---
+++ Notes.Library/Notes.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using Note.Services;
+using Notes.Library;
+
+SqlDataConnector newDataConnector = new SqlDataConnector();
+
+var result = await newDataConnector.GetAllNotesAsync();
+
+Console.ReadLine();
