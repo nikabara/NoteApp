ad7c330fb6c750e4a3eb5fe90aca6b81e8f75969
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-10 00:30:20 +0400

    Error logs debugged

---
+++ Notes.Library/Notes.Library/NotesErrorLog.cs
@@ -0,0 +1,12 @@
+﻿namespace Notes.Library
+{
+    public class NotesErrorLog
+    {
+        public int ErrorId { get; set; }
+        public int ErrorNumber { get; set; }
+        public byte ErrorState { get; set; }
+        public byte ErrorSeverity { get; set; }
+        public string? ErrorMessage { get; set; }
+        public int ErrorLine { get; set; }
+    }
+}
