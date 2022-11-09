44f54c8aefa1c5569f9acc031e2d43e8c26275b7
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-10 00:29:33 +0400

    Error log function

--- Notes.Library/Note.Services/Interfaces/IDataConnection.cs
+++ Notes.Library/Note.Services/Interfaces/IDataConnection.cs
@@ -4,6 +4,15 @@ namespace Note.Services.Interfaces
 {
     public interface IDataConnection
     {
+        /// <summary>
+        /// Returns all Notes from SQL database
+        /// </summary>
+        /// <returns></returns>
         public Task<List<NotesCS>> GetAllNotesAsync();
+        /// <summary>
+        /// Returns all Errors occured in Notes DB from SQL database
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<NotesErrorLog>> GetAllErrorLogAsync();
     }
 }
